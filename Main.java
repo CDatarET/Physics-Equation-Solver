cab8e7e4b01d4ef1d069e91a55b4b06f30c22412
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-03-15 21:57:13 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class Physics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
@@ -47,23 +47,63 @@ class Main {
                 char vr = scan.next().charAt(0);
                 
                 if(vr == 'l'){
-                    
+                    System.out.println("Enter h in j-s (ex. 1.00e-21)");
+                    double h = scan.nextDouble();
+                    System.out.println("Enter m in kg:");
+                    double m = scan.nextDouble();
+                    System.out.println("Enter v in m/s:");
+                    double v = scan.nextDouble();
+
+                    System.out.println("l = " + (h/(m*v)));
                 }
                 else if(vr == 'm'){
-                    
+                    System.out.println("Enter h in j-s (ex. 1.00e-21)");
+                    double h = scan.nextDouble();
+                    System.out.println("Enter l in m: (ex. 1.00e-9)");
+                    double l = scan.nextDouble();
+                    System.out.println("Enter v in m/s:");
+                    double v = scan.nextDouble();
+
+                    System.out.println(("m = " + (h/v)*l));
                 }
                 else if(vr == 'v'){
+                    System.out.println("Enter h in j-s (ex. 1.00e-21)");
+                    double h = scan.nextDouble();
+                    System.out.println("Enter m in kg:");
+                    double m = scan.nextDouble();
+                    System.out.println("Enter l in m: (ex. 1.00e-9)");
+                    double l = scan.nextDouble();
+
+                    System.out.println("v = " + (h/(m*l)));
                     
                 }
                 else if(vr == 'h'){
-                    
+                    System.out.println("Enter m in kg:");
+                    double m = scan.nextDouble();
+                    System.out.println("Enter l in m: (ex. 1.00e-9)");
+                    double l = scan.nextDouble();
+                    System.out.println("Enter v in m/s:");
+                    double v = scan.nextDouble();
+
+                    System.out.println("h = " + (m*v*l));
                 }
                 else{
                     System.out.println("Invalid Variable");
                 }
             }
             else if(equ == 3){
-                
+                final double h = Math.pow(6.626e-34, 2);
+                System.out.println("Enter n");
+                int n = scan.nextInt();
+                System.out.println("Enter m in kg");
+                double m = scan.nextDouble();
+                System.out.println("Enter L in m");
+                double L = scan.nextDouble();
+                L = Math.pow(L,2);
+
+                for(int i = 1; i <= n; i++){
+                    System.out.println("E"+ i + " = " + ((Math.pow(i,2)*h)/(8*m*L)));
+                }
             }
             else{
                 System.out.println("Not a valid operation");
